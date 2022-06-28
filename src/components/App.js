f7c4349f75621b71f948726c9cf1898500fca5f0
unknown <chepurnojv@gmail.com>
2022-06-28 14:46:14 +0300

    addlogitem-component

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import LogItem from "./LogItem";
+import AddLogItem from "./AddLogItem";
+
+const App = () => {
+  const [logs, setLogs] = useState([
+    {
+      _id: 1,
+      text: "This is log one",
+      priority: "low",
+      user: "Vlad",
+      created: new Date().toString(),
+    },
+    {
+      _id: 2,
+      text: "This is log two",
+      priority: "moderate",
+      user: "Brad",
+      created: new Date().toString(),
+    },
+    {
+      _id: 3,
+      text: "This is log three",
+      priority: "high",
+      user: "Alan",
+      created: new Date().toString(),
+    },
+  ]);
+
+  return (
+    <Container>
+      <AddLogItem />
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log) => (
+            <LogItem key={log._id} log={log} />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
